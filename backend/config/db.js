2da448a036e8ca0d92e0cb72309b4636c7d5c706
pchaubey25 <chaubeyp@gmail.com>
2024-10-08 09:19:25 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
+const mongoURI = 'mongodb+srv://chaubeyp:ConsTrack360@veerive.tta8g.mongodb.net/veerive-db?retryWrites=true&w=majority&appName=veerive';
 
 const configDB = async () => {
-
-    const dbConnection = await mongoose.connect('mongodb+srv://chaubeyp:ConsTrack360@veerive.tta8g.mongodb.net/?retryWrites=true&w=majority&appName=veerive')
-    const dbName = dbConnection.connection.name
-    console.log('connected to database', dbName)
-}
+    try {
+        const dbConnection = await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        const dbName = dbConnection.connection.name
+        console.log('connected to database', dbName)
+        } catch (err) {
+        console.error('Failed to connect to database', err);
+      }
+    }
 
 export default configDB
