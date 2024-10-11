7770d91f15633a853a4e3326916f32b88504414b
pchaubey25 <chaubeyp@gmail.com>
2024-10-11 13:02:30 +0530

    Update axios.js

--- cms/src/config/axios.js
+++ cms/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create ({
-    baseURL: 'http://localhost:3030'
+    baseURL: 'https://veeriveoct5.onrender.com'
 })
 
 // export default axios.create ({
