05b4f1b2589962e69cd7e18b24f100ac617bb524
pchaubey25 <chaubeyp@gmail.com>
2024-10-13 14:34:31 +0530

    Update axios.js

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios'
 
 export default axios.create ({
  //   baseURL: 'http://localhost:3030'
-      baseURL: 'https://veerive-frontend.vercel.app'
+      baseURL: 'https://veeriveoct5.onrender.com'
 })
