3fe48f3052d21f1df17da8f05b8a527c0d9fcfda
pchaubey25 <chaubeyp@gmail.com>
2024-10-21 12:27:10 +0530

    Update source-model.js

--- backend/app/models/source-model.js
+++ backend/app/models/source-model.js
@@ -2,7 +2,7 @@ import {Schema, model} from 'mongoose'
 
 const sourceSchema = new Schema ({
     sourceName: { type: String, required: true },
-    sourceType: { type: String, enum: ['News Site', 'Influencer', 'Professional Services Firm', 'Blog Site', 'Other'], required: true },
+    sourceType: { type: String, enum: ['News Site', 'Social Media Post', 'Professional Services Firm', 'Blog Site', 'Other'], required: true },
     generalComment: { type: String, required: false },
 }, {timestamps: true})
 
