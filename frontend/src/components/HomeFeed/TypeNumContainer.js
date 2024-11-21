7e85cd796ec6832b86a6fcbbaea6f1a8ee5e7294
pchaubey25 <chaubeyp@gmail.com>
2024-11-21 09:51:54 +0530

    Update TypeNumContainer.js

--- frontend/src/components/HomeFeed/TypeNumContainer.js
+++ frontend/src/components/HomeFeed/TypeNumContainer.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../../html/css/TypeNumContainer.css'; // Import the CSS file
+import { HomeFeedContext } from '../../context/HomeFeedContext';
 
-export default function TypeNumContainer({
-  dataForTypeNum,
-  summary
-}) {
+export default function TypeNumContainer({context}) {
+  //const { sectorMap, subSectorMap } = useContext(HomeFeedContext); // Access the maps from context
+//  const { contextTitle, dataForTypeNum, summary } = context;
+
+  // Map the IDs to their respective names
+  //const sectorNames = sectors.map(id => sectorMap[id]).filter(Boolean);
+  //const subSectorNames = subSectors.map(id => subSectorMap[id]).filter(Boolean);
+
+  // Join the sector and subSector names with "|"
+  //const categories = `${sectorNames.join(' | ')} | ${subSectorNames.join(' | ')}`;
+
   return (
     <div className="container">
       <div className="content">
         <div className="left-column">
           {/* Display dynamic big text */}
-          <div className="big-text">{dataForTypeNum}</div>
+          <div className="big-text">{'$ XX Billion'}</div>
         </div>
         <div className="right-column">
-          {/* Display dynamic summary */}
-          <div className="bullet-list" dangerouslySetInnerHTML={{ __html: summary }} />
+          {/* Display dynamic summary --  <div className="bullet-list" dangerouslySetInnerHTML={{ __html: summary }} />*/}
+          <div className="bullet-list" dangerouslySetInnerHTML={{ __html: 'Lorem Ipsum'}}/>
 
         </div>
       </div>
